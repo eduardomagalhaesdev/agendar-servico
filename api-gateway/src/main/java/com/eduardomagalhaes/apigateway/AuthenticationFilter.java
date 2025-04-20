82aefa240cbb37f5f83445b074f3922a92951bff
eduardomagalhaesdev <eduardomagalhaes.dev@gmail.com>
2025-04-20 15:48:21 -0300

    commit inicial api-gateway

---
+++ api-gateway/src/main/java/com/eduardomagalhaes/apigateway/AuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.eduardomagalhaes.apigateway;
+
+import org.springframework.http.HttpHeaders;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthenticationFilter implements WebFilter {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+
+        try {
+            Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        } catch (Exception e) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+}
